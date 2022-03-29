e0cd0acc884916178a1d0eb49e897b206ec49a1f
Zheng <aheze@getfind.app>
2022-03-28 22:04:36 -0700

    Initial Commit

---
+++ Buildings/BuildingsApp.swift
@@ -0,0 +1,19 @@
+//
+//  BuildingsApp.swift
+//  Buildings
+//
+//  Created by A. Zheng (github.com/aheze) on 3/28/22.
+//  Copyright © 2022 A. Zheng. All rights reserved.
+//
+    
+
+import SwiftUI
+
+@main
+struct BuildingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
